cbe22b5e98f6f8e30dc6dde0f5586ad3c4921c6e
Nikhil <nikhilshah.net@hotmail.com>
2023-07-26 11:19:00 +0530

    Day 2 - Inmemory Collection - CRUD

---
+++ Day 2/shoppingAPI_inMemoryCollection/shoppingAPI_inMemoryCollection/Controllers/ProductsController.cs
@@ -0,0 +1,114 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using shoppingAPI_inMemoryCollection.Models;
+
+namespace shoppingAPI_inMemoryCollection.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        Products pObj = new Products(); // we should use dependency injection instead of createing new product
+
+
+        [HttpGet]
+        [Route("/products")]
+        public IActionResult GetAllProduct()
+        {
+            return Ok(pObj.GetAllProducts());
+        }
+
+        [HttpGet]
+        [Route("/products/category/{category}")]
+        public IActionResult GetAllProductsByCategory(string category)
+        {
+            try
+            {
+                return Ok(pObj.GetProductsByCategory(category));
+            }
+            catch (Exception es)
+            {
+                return NotFound(es.Message);                
+            }
+        }
+
+        [HttpGet]
+        [Route("/products/id/{id}")]
+        public IActionResult GetProductById(int id)
+        {
+            try
+            {
+                return Ok(pObj.GetProductById(id));
+            }
+            catch (Exception es)
+            {
+                return NotFound(es.Message);
+            }
+        }
+
+
+        [HttpGet]
+        [Route("/products/avability/{yesorno}")]
+        public IActionResult GetProductInStock(bool yesorno)
+        {
+            try
+            {
+                return Ok(pObj.GetProductsInStock(yesorno));
+            }
+            catch (Exception es)
+            {
+
+                return NotFound(es.Message);
+            }
+        }
+
+        [HttpGet]
+        [Route("/products/count")]
+        public IActionResult GettotalProducts()
+        {
+            return Ok(pObj.GetTotalProducts());
+        }
+
+        [HttpPost]
+        [Route("/products/add")]
+        public IActionResult AddnewProduct(Products newProduct)
+        {
+           string result =  pObj.AddNewProduct(newProduct);
+            return Created("", result);
+        }
+        [HttpDelete]
+        [Route("/products/delete/{id}")]
+        public IActionResult DeleteProduct(int id)
+        {
+            try
+            {
+                string result = pObj.DeleteProduct(id);
+                return Accepted(result);
+            }
+            catch(Exception es)
+            {
+                return NotFound(es.Message);
+            }
+        }
+        [HttpPut]
+        [Route("/products/update")]
+        public IActionResult UpdateProduct(Products changes)
+        {
+            try
+            {
+                string result = pObj.UpdateProduct(changes);
+                return Accepted(result);
+            }
+            catch (Exception es)
+            {
+                return NotFound(es.Message);
+            }
+        }
+
+    }
+}
+
+
+
+
+
---
+++ Day 2/shoppingAPI_inMemoryCollection/shoppingAPI_inMemoryCollection/Models/Products.cs
@@ -0,0 +1,140 @@
+﻿using System.Security.Cryptography;
+
+namespace shoppingAPI_inMemoryCollection.Models
+{
+    public class Products
+    {
+
+        #region Properties
+        public int pId { get; set; }
+        public string pName { get; set; }
+        public string pCategory { get; set; }
+        public string pDescription { get; set; }
+        public double pPrice { get; set; }
+        public bool pIsInStock { get; set; }
+        #endregion
+
+        #region Data
+
+        static List<Products> pList = new List<Products>()
+        {
+            new Products(){ pId=101, pName="Pepsi", pCategory="Cold-Drink", pDescription="Cool black color water to kill the thurst", pIsInStock=true, pPrice=70},
+            new Products(){ pId=102, pName="IPhone", pCategory="Smart Phone", pDescription="Make Smart calls and videos", pIsInStock=true, pPrice=150000},
+            new Products(){ pId=103, pName="Fanta", pCategory="Cold-Drink", pDescription="Cool Orange color water to kill the thurst", pIsInStock=false, pPrice=90},
+            new Products(){ pId=104, pName="Maggie", pCategory="Fast-Food", pDescription="Quick Junf food", pIsInStock=true, pPrice=150},
+            new Products(){ pId=105, pName="Dell Lattitude", pCategory="Electronics", pDescription="Attend a training", pIsInStock=true, pPrice=75000},
+            new Products(){ pId=106, pName="Air-Pods", pCategory="Electronics", pDescription="Listen Well", pIsInStock=true, pPrice=26000},
+            new Products(){ pId=107, pName="Boat Headphones", pCategory="Electronics", pDescription="Listen well", pIsInStock=false, pPrice=3500},
+            new Products(){ pId=108, pName="Fossil Q", pCategory="Electronics", pDescription="Check the time", pIsInStock=true, pPrice=35000},
+            new Products(){ pId=109, pName="Samsung AC", pCategory="Electronics", pDescription="Feel Cold", pIsInStock=false, pPrice=48000},
+
+        };
+
+        #endregion
+
+        #region Methods
+
+            #region Get methods
+        public List<Products> GetAllProducts()
+        {
+            return pList;
+        }
+
+        public Products GetProductById(int pid)
+        {
+            var prod = pList.Find(p => p.pId == pid);
+            if (prod != null)
+            {
+                return prod;
+            }
+            else
+            {
+                throw new Exception("Product Not found");
+            }
+        }
+
+        public List<Products> GetProductsByCategory(string category)
+        {
+            var products = pList.FindAll(p => p.pCategory == category);
+            if (products.Count > 0)
+            {
+                return products;
+            }
+            else
+            {
+                throw new Exception("No Product for this category");
+            }
+        }
+
+        public List<Products> GetProductsInStock(bool yesorno)
+        {
+            var products = pList.FindAll(p => p.pIsInStock == yesorno);
+            if (products.Count > 0)
+            {
+                return products;
+            }
+            else
+            {
+                throw new Exception("Please pass yes or no only");
+            }
+        }
+
+        public int GetTotalProducts()
+        {
+            return pList.Count;
+        }
+
+        
+        #endregion
+
+           #region Add Update and delete
+        public string AddNewProduct(Products newProduct)
+        {
+            pList.Add(newProduct);
+            return "Product Added Successfully";
+        }
+
+        public string UpdateProduct(Products changes)
+        {
+            var prod = pList.Find(p => p.pId == changes.pId);
+            if (prod != null)
+            {
+                prod.pName = changes.pName;
+                prod.pCategory = changes.pCategory;
+                prod.pPrice = changes.pPrice;
+                prod.pIsInStock = changes.pIsInStock;
+                return "Product Updated Successfully";
+            }
+            else
+            {
+                throw new Exception("Product Not found");
+            }
+        }
+
+        public string DeleteProduct(int pid)
+        {
+            var prod = pList.Find(p => p.pId == pid);
+            if (prod != null)
+            {
+                pList.Remove(prod);
+                return "Product Deleted Successfully";
+            }
+            else
+            {
+                throw new Exception("Product Not found");
+            }
+        }
+        #endregion
+
+
+        #endregion
+
+    }
+}
+
+
+
+
+
+
+
