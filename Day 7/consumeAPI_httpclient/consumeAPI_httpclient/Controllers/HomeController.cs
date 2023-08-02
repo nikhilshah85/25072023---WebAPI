f47022065455be12a0a61d533a926815fbf6f8ef
Nikhil <nikhilshah.net@hotmail.com>
2023-08-02 11:11:53 +0530

    Day 6 - HttpClient Calls

---
+++ Day 7/consumeAPI_httpclient/consumeAPI_httpclient/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using consumeAPI_httpclient.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace consumeAPI_httpclient.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+
+        public IActionResult DisplayPost()
+        {
+            PostDetails postObj = new PostDetails();
+            ViewBag.post = postObj.GetPostData();
+            return View();
+        }
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Day 7/consumeAPI_httpclient/consumeAPI_httpclient/Models/Products.cs
@@ -0,0 +1,43 @@
+﻿namespace consumeAPI_httpclient.Models
+{
+    public class Products
+    {
+        public int id { get; set; }
+        public string title { get; set; }
+        public int price { get; set; }
+        public string description { get; set; }
+        public string category { get; set; }
+        public string image { get; set; }
+
+        public List<Products> productList = new List<Products>();
+
+        public List<Products> GetPostData()
+        {
+            HttpClient client = new HttpClient();
+
+            client.DefaultRequestHeaders.Accept.Clear(); //clear the browser tab setting
+            client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+
+            string url = "https://jsonplaceholder.typicode.com/posts";
+
+            var response = client.GetAsync(url).Result;
+
+            if (response.IsSuccessStatusCode)
+            {
+                var data = response.Content.ReadAsAsync<List<PostDetails>>();
+                data.Wait();
+                productList = data.Result;
+                return productList;
+
+            }
+            else
+            {
+                throw new Exception("Could not get data from server, please contact Admin");
+            }
+
+        }
+    }
+}
+
+    
+
