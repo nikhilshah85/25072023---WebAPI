0aad2d8185cdd79aa038e1a707280e0184ad7979
Nikhil <nikhilshah.net@hotmail.com>
2023-08-04 10:44:53 +0530

    Day 8 - CORS

---
+++ Day 8/CORS/consume_api_withCORS/consume_api_withCORS/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace consume_api_withCORS.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
